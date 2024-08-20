7cc62ad39898d173b8909585835088dc0cff2b7e
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-08-20 10:25:59 -0300

    Finalização do Desafio DIO

--- src/Contador.java
+++ src/Contador.java
@@ -1,4 +1,42 @@
-public class Contador {
+import java.util.Scanner;
 
+public class Contador {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parametro.: ");
+        int parametroUm = scanner.nextInt();
+
+        System.out.println("Digite o segundo parametro.: ");
+        int parametroDois = scanner.nextInt();
+
+        try{
+
+            contar(parametroUm, parametroDois);  
+            
+        } catch(ParametrosInvalidosException e){
+            
+            System.out.println("O segundo parametro não pode ser maior que o primeiro.");
+            
+        }
+        scanner.close();
+    }
     
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        
+        int contagem;
+
+        if (parametroDois < parametroUm){
+            throw  new ParametrosInvalidosException("O segundo parametro não pode ser maior que o primerio.");
+        }else{
+           contagem = parametroDois - parametroUm;
+        }
+        for(int cont = 0; cont <= contagem; cont++){
+            System.out.println("Imprimindo o número " + cont);
+        }
+    }
+    
+  
+   
 }
--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,3 +1,6 @@
-public class ParametrosInvalidosException {
 
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
 }
